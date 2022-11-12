f6d3469e911ba57534d3d984c13ff78e003d4eac
Владислав Усманов <vladislav.usmanov@nordclan.com>
2022-11-12 23:25:02 +0300

    feat: add Pinia and Vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
+
+const pinia = createPinia()
+app.use(pinia)
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+app.use(vuetify)
 
 app.mount('#app')
