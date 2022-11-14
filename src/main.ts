be54a611f40a105eddaa0c59ce8e4146270717bf
Владислав Усманов <vladislav.usmanov@nordclan.com>
2022-11-14 20:17:48 +0300

    feat: setup app theme

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,31 @@ import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import { createPinia } from 'pinia';
-import { createVuetify } from 'vuetify';
+import { createVuetify, ThemeDefinition } from 'vuetify';
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from '@/routes';
+import { md3 } from 'vuetify/blueprints';
 
 const app = createApp(App);
 
 const pinia = createPinia();
 app.use(pinia);
+
+const appTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    primary: '#80489C',
+    'primary-darken-1': '#432C7A',
+    secondary: '#FF8FB1',
+    'secondary-darken-1': '#FCE2DB',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+};
 
 const vuetify = createVuetify({
   components,
@@ -22,6 +39,13 @@ const vuetify = createVuetify({
     aliases,
     sets: {
       mdi,
+    },
+  },
+  blueprint: md3,
+  theme: {
+    defaultTheme: 'appTheme',
+    themes: {
+      appTheme,
     },
   },
 });
