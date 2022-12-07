ce471f518e680dfe371d93f334184eec19a39a7b
kritzona <hangookhelly@gmail.com>
2022-12-07 11:12:04 +0300

    refactor: playlist tracks

--- src/components/Playlist/Playlist.vue
+++ src/components/Playlist/Playlist.vue
@@ -1,22 +1,14 @@
 <template>
   <v-card :title="name">
     <template #text>
-      <v-list lines="two">
-        <PlaylistTrack
-          v-for="track in tracks"
-          :key="track.id"
-          :name="track.name"
-          :author="track.author"
-          :cover="track.cover"
-        />
-      </v-list>
+      <PlaylistTracks :tracks="tracks" />
     </template>
   </v-card>
 </template>
 
 <script lang="ts" setup>
 import { PlaylistModel } from '@/models/playlist.model';
-import PlaylistTrack from './PlaylistTrack.vue';
+import PlaylistTracks from './PlaylistTracks.vue';
 
 defineProps<{
   name: PlaylistModel['name'];
---
+++ src/components/Playlist/PlaylistTracks.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-list lines="two">
+    <PlaylistTrack
+      v-for="track in tracks"
+      :key="track.id"
+      :name="track.name"
+      :author="track.author"
+      :cover="track.cover"
+    />
+  </v-list>
+</template>
+
+<script lang="ts" setup>
+import { PlaylistModel } from '@/models/playlist.model';
+import PlaylistTrack from './PlaylistTrack.vue';
+
+defineProps<{
+  tracks: PlaylistModel['tracks'];
+}>();
+</script>
