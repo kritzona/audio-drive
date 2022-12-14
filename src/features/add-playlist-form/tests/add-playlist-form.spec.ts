3fe8016ae1d7a6e0d4a1b9154a337599f7ad91e3
kritzona <hangookhelly@gmail.com>
2022-12-14 17:04:22 +0300

    test: fix add-playlist-form test

--- src/features/add-playlist-form/tests/add-playlist-form.spec.ts
+++ src/features/add-playlist-form/tests/add-playlist-form.spec.ts
@@ -2,24 +2,50 @@ import { describe, it, expect, beforeEach } from 'vitest';
 import { mount } from '@vue/test-utils';
 import AddPlaylistForm from '../ui/AddPlaylistForm.vue';
 import * as providers from '@/app/providers';
-import { Pinia } from 'pinia';
+import { createTestingPinia } from '@pinia/testing';
+import { useUserPlaylistStore } from '@/widgets/playlists';
 
 describe('AddPlaylistForm.vue', () => {
   let vuetify: any;
-  let pinia: Pinia;
 
   beforeEach(() => {
     vuetify = providers.shallowVuetify;
-    pinia = providers.pinia;
   });
 
   it('render', () => {
     const wrapper = mount(AddPlaylistForm, {
       global: {
-        plugins: [vuetify, pinia],
+        plugins: [vuetify, createTestingPinia()],
       },
     });
 
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('validation failure', async () => {
+    const wrapper = mount(AddPlaylistForm, {
+      global: {
+        plugins: [vuetify, createTestingPinia()],
+      },
+    });
+
+    await wrapper.find('input[name="name"]').setValue('');
+
+    await expect(wrapper.vm.handleSubmit()).rejects.toEqual(undefined);
+  });
+
+  it('successful validation', async () => {
+    const wrapper = mount(AddPlaylistForm, {
+      global: {
+        plugins: [vuetify, createTestingPinia()],
+      },
+    });
+
+    const userPlaylistStore = useUserPlaylistStore();
+
+    await wrapper.find('input[name="name"]').setValue('Тестовый плейлист');
+    await wrapper.vm.handleSubmit();
+
+    expect(userPlaylistStore.add).toHaveBeenCalledOnce();
   });
 });
