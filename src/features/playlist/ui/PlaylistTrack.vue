7b9b367875bb63135b5b60060b767c88be5b8f27
kritzona <hangookhelly@gmail.com>
2022-12-21 15:32:40 +0300

    feat: add controls to playlist tracks

--- src/features/playlist/ui/PlaylistTrack.vue
+++ src/features/playlist/ui/PlaylistTrack.vue
@@ -4,16 +4,47 @@
     :value="id"
     :subtitle="author"
     :prepend-avatar="cover"
-  />
+  >
+    <template #append>
+      <template v-if="isCurrentTrack">
+        <SharedUI.PlayButton
+          v-if="!audioStore.playing"
+          @click.stop="handlePlayButtonClick"
+        />
+        <SharedUI.PauseButton v-else @click.stop="handlePauseButtonClick" />
+      </template>
+      <template v-else>
+        <SharedUI.PlayButton @click="handlePlayButtonClick" />
+      </template>
+    </template>
+  </v-list-item>
 </template>
 
 <script lang="ts" setup>
-import { AudioModel } from '@/entities/audio';
+import { AudioModel, useAudioStore } from '@/entities/audio';
+import SharedUI from '@/shared/ui';
+import { computed } from 'vue';
 
-defineProps<{
+const props = defineProps<{
   id: AudioModel['id'];
   name: AudioModel['name'];
   author: AudioModel['author'];
   cover: AudioModel['cover'];
 }>();
+
+const audioStore = useAudioStore();
+
+const isCurrentTrack = computed(() => props.id === audioStore.audio?.id);
+
+const handlePlayButtonClick = () => {
+  if (!isCurrentTrack.value) {
+    return;
+  }
+
+  audioStore.play();
+};
+
+const handlePauseButtonClick = () => {
+  audioStore.pause();
+};
 </script>
